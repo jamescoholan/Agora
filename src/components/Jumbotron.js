ebd23c56346334b557e8e81a5789bfe0ba504c0a
savyungblanc <syb@sybcreative.com>
2020-08-19 14:26:03 -0400

    Update Jumbotron.js

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -18,7 +18,7 @@ const Jumbotron = ({ children }) => (
     <div className="synopsis">
       
 
-      <p>Welcome To Agora!!!</p>
+      <p>Agora Welcomes YOU!!!!</p>
 
       <Button icon="play">Play</Button>
       <Button icon="info-circle">Info</Button>
